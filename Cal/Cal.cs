ad0043d911ab49b74b31eb293bf1c7dae83fe79a
jnishida <nishida@allora-inc.jp>
2019-07-27 22:41:45 +0900

    String=>string

--- Cal/Cal.cs
+++ Cal/Cal.cs
@@ -29,7 +29,7 @@ namespace Cal
 
         private static void CalBody(DateTime start)
         {
-            Console.WriteLine(String.Format("{0}年{1}月", start.Year, start.Month));
+            Console.WriteLine(string.Format("{0}年{1}月", start.Year, start.Month));
             Console.WriteLine(" 日 月 火 水 木 金 土");
             for (DayOfWeek d = DayOfWeek.Sunday; d < start.DayOfWeek; d++)
             {
@@ -37,7 +37,7 @@ namespace Cal
             }
             for (DateTime d = start; d < start.AddMonths(1); d = d.AddDays(1))
             {
-                Console.Write(String.Format("{0, 3}", d.Day));
+                Console.Write(string.Format("{0, 3}", d.Day));
                 if (d.DayOfWeek == DayOfWeek.Saturday) {
                     Console.WriteLine();
                 }
